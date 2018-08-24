3915e3f2e2453122f751fe0b9d04eda0f503dd2d
Korkmatik <9c54efc1-143b-4f5f-9d2f-f6f189583f0f@aleeas.com>
2018-08-24 23:47:27 +0200

    Day 11

--- Cgi_Programmierung/cgi-bin/chat.py
+++ Cgi_Programmierung/cgi-bin/chat.py
@@ -2,19 +2,29 @@
 
 HTTP_SCHABLONE = '''Content-Type: text/html
 
-<html><head><title>Python Chat</title></head>
+<html>
+    <meta http-equiv="Content-Type" content="charset=utf-8" />
+    <head>
+        <title>Python Chat</title>        
+    </head>
     <body>
         <h1>Python Chat</h1>
         {} <hr>
         <form action="http://localhost/cgi-bin/chat.py" method="POST">
             <input type="hidden" name="username" value="{}">
             Ich sage: <input type="text" name="message" size="40" maxlenght="50">
-            <input type="Submit" value="OK">
+            <input type="Submit" value="OK"><br>
+            <input type="radio" name="volume" value="normal" checked="checked"> normal
+            <input type="radio" name="volume" value="shout"> schreiend
+            <input type="radio" name="volume" value="whisper"> fl&uuml;sternd
         </form>
     </body>
 </html>'''
 
 PATH = 'Chat/dialog.txt'
+HTML = {ord("ä"): "&auml;", ord("ö"): "&ouml;", ord("ü"): "&uuml;",
+        ord("Ä"): "&Auml;", ord("Ö"): "&Ouml;", ord("Ü"): "&Uuml;",
+        ord("ß"): "&szlig;"}
 
 import cgi, cgitb
 cgitb.enable()
@@ -31,11 +41,18 @@ class Dialog:
             f = open(file, 'w')
             f.close()
 
-    def update(self, name, message):
+    def update(self, name, message, volume):
         """This function adds a new message into the Dialog"""
         if len(self.text) > 10:
             self.text = self.text[-10:]
-        newLine = name + ': ' + message + '<br>'
+            
+        if volume == 'whisper':
+            newLine = name + ': <font size="-2" color="#d3d3d3">' + message + '</font><br>'
+        elif volume == 'shout':
+            newLine = name + ': <font size="+2" color="#FF0000">' + message + '</font><br>'
+        elif volume =='normal':
+            newLine = name + ': <font size="+1" color="#000}">' + message + '</font><br>'   
+
         self.text.append(newLine)
         f = open(self.file, 'w')
         for l in self.text:
@@ -55,8 +72,10 @@ class Chatroom:
         self.dialog = Dialog(PATH)
         self.message = self.form.getvalue('message')
         self.username = self.form.getvalue('username')
+
+        self.volume = self.form.getvalue('volume')
         if self.message:
-            self.dialog.update(self.username, self.message)
+            self.dialog.update(self.username.translate(HTML), self.message.translate(HTML), self.volume)
 
     def __str__(self):
         return HTTP_SCHABLONE.format(self.dialog, self.username)
